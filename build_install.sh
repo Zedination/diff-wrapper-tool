85bafa41dff2cdc20e874c391600f56aae2a1f3b
Zedination <50323429+Zedination@users.noreply.github.com>
2023-05-24 01:44:35 +0700

    updaet build srcipt

--- build_install.sh
+++ build_install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 mvn clean install
-jpackage --name TestJavaFXApp --input "D:\Source Code\JavaFX Project\diff-wrapper-tool\target" --module-path "D:\Java\openjfx-19.0.2.1_windows-x64_bin-jmods\javafx-jmods-19.0.2.1" --main-jar diff-wrapper-tool-1.0-SNAPSHOT-shaded.jar --main-class com.zedination.diffwrappertool.Main1 --type exe --win-shortcut --win-menu
+jpackage --name DiffWrapperTool --input "D:\Source Code\JavaFX Project\diff-wrapper-tool\target" --module-path "D:\Java\openjfx-19.0.2.1_windows-x64_bin-jmods\javafx-jmods-19.0.2.1" --main-jar diff-wrapper-tool-1.0-SNAPSHOT-shaded.jar --main-class com.zedination.diffwrappertool.Main1 --type exe --win-shortcut --win-menu
